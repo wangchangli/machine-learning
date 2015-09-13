bcb7b30a1736c1ec6ca69e9afbd5a97ea015bce3
长历王 <changliwang@changliwangdeMacBook-Pro.local>
2015-09-13 13:05:03 +0800

    ex8

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -42,14 +42,21 @@ Theta_grad = zeros(size(Theta));
 
 
 J = sum(sum((((Theta*X')'-Y).*R).^2))/2;
+J = J + (sum(sum(Theta.^2)) + sum(sum(X.^2)))*lambda/2;
 
-for i=1:num_movies:
-	X_grad(i) =                                                                                                                        
+for i=1:num_movies      
+  idx = find(R(i,:)==1);
+  Theta_temp = Theta(idx,:);
+  Y_temp = Y(i,idx);  
+  X_grad(i,:) = (X(i,:)*Theta_temp'-Y_temp) * Theta_temp + lambda*X(i,:);                                                                                                                       
 end
 
-
-
-
+for j=1:num_users
+  idx = find(R(:,j)==1);
+  X_temp = X(idx,:);
+  Y_temp = Y(idx,j);
+  Theta_grad(j,:) = ((X_temp*Theta(j,:)'-Y_temp))' * X_temp + lambda*Theta(j,:);
+end
 
 
 % =============================================================
